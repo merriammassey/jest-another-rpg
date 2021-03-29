5e145e8a6cd2985ae9d9bf9ac986361e1a4c147b
Maria M <prov21_1@hotmail.com>
2021-03-29 16:02:18 -0700

    potion test

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,9 @@
+//require potion module 
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
